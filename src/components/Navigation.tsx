1d75d89a74ac912a80625f31e79b992d29e04093
quentin <qchallon@dev-id.fr>
2026-02-10 17:53:14 +0100

    fix: change name into the main button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -25,7 +25,7 @@ export function Navigation() {
         <div className="flex items-center justify-between">
           <a href="#" className="text-2xl tracking-tight">
             <span className="bg-primary text-secondary dark:bg-[#2C3E50] dark:text-secondary px-4 py-2 rounded-lg">
-              Your Name
+              Quentin Challon
             </span>
           </a>
 
